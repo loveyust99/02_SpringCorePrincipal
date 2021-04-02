d32ce8adb68d8e37bc4786166c193706cd0d235f
loveyust99 <loveyust99@gmail.com>
2021-04-02 20:10:42 +0900

    주문과 할인 도메인 설계/개발 및 테스트 (적용내역: 관심사 분리 즉 역할인 인터페이스만 바라보게 하고, 구현 객체를 생성하고 연결하는 책임을 가지는 별도의 설정 클래스인 AppConfig.class(IoC컨테이너 역할)를 만들어 OCP/DIP와 같은 객체지향 설계원칙인 SOLID를 준수하게 적용함. 참고: 현 시점까지 Spring의 개입 없이 순수 Java코드로 작성됨.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -12,19 +12,23 @@ import hello.core.order.service.OrderServiceImpl;
 
 public class AppConfig {
 
+    //멤버서비스 역할(멤버 가입/조회 등)을 수행하는 것에만 집중.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    //주문서비스 역할(상품 주문 등)을 수행하는 것에만 집중. 별도 서비스인 상품 조회는 현재 구현하지 않음.
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    //저장소(메모리DB, Oracle/MS-SQL/MySQL/Redis/MongoDB 등 상관없이)의 역할(CRUD)에만 집중.
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
         //return new JDBCMemberRepository();
     }
 
+    //할인정책 역할(정액 또는 정률)에만 집중.
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
         //return new RateDiscountPolicy();
