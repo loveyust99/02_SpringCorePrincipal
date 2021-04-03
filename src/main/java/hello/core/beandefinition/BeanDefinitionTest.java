a5494b985375d3a29bf481a35b77825991487898
loveyust99 <loveyust99@gmail.com>
2021-04-03 21:16:56 +0900

    Spring Bean 설정 메타정보 - BeanDefinition(추상화)을 통한 다양한 설정 형식(Java코드, XML, JSON 등) 지원

---
+++ src/main/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,32 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.springframework.beans.factory.config.BeanDefinition.ROLE_APPLICATION;
+
+public class BeanDefinitionTest {
+
+//    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig .class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("AppConfig.xml");
+
+    @Test
+    @DisplayName("Spring Bean 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        ", beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+
+}
