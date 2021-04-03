a9cafc25485f2b7aaac8cf0745ac2540d3f75ba6
loveyust99 <loveyust99@gmail.com>
2021-04-03 23:21:05 +0900

    Spring 컨테이너(DI컨테이너)에 의해 객체 인스턴스가 싱글톤으로 관리된다. 순수 Java코드로 싱글톤을 구현할 때의 복잡한 코드 등 작성이 필요없으며 싱글톤의 단점이 보완된다. 단 스프링의 기본 빈 등록방식은 싱글톤이지만 싱글톤 방식만 지원하는 것은 아니며 요청할 때마다 새로운 객체를 생성해서 반환하는 기능도 제공한다.

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.service.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -36,6 +38,25 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("Spring 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회 : 호출할 때마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        //2. 조회 : 호출할 때마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 == memberService2
+        //즉, Spring 컨테이너(DI컨테이너)에 의해 객체 인스턴스가 싱글톤으로 관리된다.
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
     }
 
 }
